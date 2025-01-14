61c9e81d684a8a3bbcf3047af3e0d6f5c8a17d1c
mathewolickal <92604851+mathewolickal@users.noreply.github.com>
2025-01-14 16:50:53 -0600

    Update streamlit_sig.py

--- streamlit_sig.py
+++ streamlit_sig.py
@@ -1,17 +1,32 @@
 from langchain_openai import ChatOpenAI
 import streamlit as st
+from langchain.prompts import PromptTemplate
 
-from langchain.globals import set_debug
+st.title("Ask the dosage/direction that needs to be converted")
 
-set_debug(True)
+with st.sidebar:
+        st.title("Provide your API Key")
+        OPENAI_API_KEY = st.text_input("OpenAI API key",type="password")
 
-OPENAI_API_KEY = "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"
+
+
+if not OPENAI_API_KEY:
+    st.info("Enter your OpenAI API key to Continue")
+    st.stop()
 
 llm = ChatOpenAI(model="gpt-4o", api_key=OPENAI_API_KEY)
-st.title("Ask Anything")
+
+prompt_template = PromptTemplate(
+    input_variables=["question"],
+    template= """ You are an expert in giving the SIG code value from a dosage Direction.
+     Only provide the SIG Value. Avoid giving information if the input is not clear and answer:I dont know
+     Answer the question: What is the SIG code for{question}?
+     """
+)
 
 question = st.text_input("Enter the Dosage/Direction")
 
 if question:
-    response = llm.invoke(question)
+    #response = llm.invoke(question)
+    response = llm.invoke(prompt_template.format(question=question))
     st.write(response.content)
